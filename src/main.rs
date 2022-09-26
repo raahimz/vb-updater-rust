31a62a55f5537fc68f0c21621f65fc98e85b8537
Raahim MAC <raahim.home@gmail.com>
2022-09-26 22:53:09 +0500

    add capability for special commands

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io::{BufRead, BufReader};
 use std::process::{Command, Stdio};
 use std::time::{Instant, Duration};
 
-const PROCESS_NAME: &str = "main.exe";
+const PROCESS_NAME: &str = "vb-rec";
 const REPLACE_WITH_STABLE_VERSION_FREQ: u32 = 3;
 const REPLACE_WITH_STABLE_VERSION_DURATION: Duration = Duration::from_secs(3600 * 24);
 
@@ -13,41 +13,20 @@ fn replace_with_stable_version() {
 fn main() {
     let mut crash_count = 0;
     let mut start_time = Instant::now();
+    let mut has_crashed = true;
+    let mut should_run = true;
 
-    while true {
-        // let mut is_running = false;
-
-        // Checking if vb-recorder-v2 process is running
-        // let output = Command::new("ps")
-        //     .arg("-a")
-        //     .output()
-        //     .expect("failed to execute process");
-
-        // let output = String::from_utf8(output.stdout).expect("invalid utf8");
-        // let output: Vec<&str> = output.split(" ").collect();
-
-        // for key in output {
-        //     let key: Vec<&str> = key.split("\n").collect();
-        //     if key[0] == PROCESS_NAME {
-        //         is_running = true
-        //     }
-        // }
-
-        // Starting vb-recorder-v2 if it isn't running
-        // if !is_running {
-        //     println!("Starting {PROCESS_NAME}");
-
-        //     let output = Command::new(format!("/home/raahim/Dev/vb-rec-v2/src/{PROCESS_NAME}"))
-        //         .output()
-        //         .expect("failed to execute process");
-        // }
-
+    while should_run {
         println!("VB-Updater: Starting {PROCESS_NAME}\n");
 
-        let mut output = Command::new(format!("C:\\Users\\Administrator\\Desktop\\Dev\\vb-rec-v2-personal\\src\\main.exe"))
+        let mut output = Command::new(format!("/Users/raahim/vb-updater-rust/src/vb-rec"))
             .stdout(Stdio::piped())
             .spawn()
             .expect("failed to execute process");
+
+        let id = output.id();
+
+        println!("{id}");
             
         let stdout = output.stdout.as_mut().unwrap();
         let stdout_reader = BufReader::new(stdout);
@@ -58,11 +37,31 @@ fn main() {
 
             println!("{}", line);
 
-            if line == "RECORDER_RESTART_REQUESTED" {
-                output.kill();
-                println!("\nVB-Updater: RECORDER_RESTART_REQUESTED - Restarting VB-Recorder\n");
+            if line == "RECORDER_RESTART_REQUESTE" {
+                println!("\nVB-Updater: RECORDER_RESTART_REQUESTED - Restarting VB-Recorder");
+                
+                // Resetting values
+                crash_count = 0;
+                start_time = Instant::now();
+                
+                has_crashed = false;
+
+                // Killing process
+                let _ = Command::new(format!("kill"))
+                    .args(["-9", &id.to_string()])
+                    .output()
+                    .expect("failed to execute process");
             } else if line == "RECORDER_STOP_REQUESTED" {
-                println!("\nVB-Updater: RECORDER_STOP_REQUESTED - Stopping VB-Recorder\n");
+                println!("\nVB-Updater: RECORDER_STOP_REQUESTED - Stopping VB-Recorder");
+
+                should_run = false;
+                has_crashed = false;
+                
+                // Killing process
+                let _ = Command::new(format!("kill"))
+                    .args(["-9", &id.to_string()])
+                    .output()
+                    .expect("failed to execute process");
             }
             
             // If duration since start has passed 24 hours, reset crash_count & start_time
@@ -76,8 +75,13 @@ fn main() {
         }
 
         println!("\nVB-Updater: {PROCESS_NAME} has stopped running...");
-        crash_count += 1;
-        println!("VB-Updater: Crash Count in last 24 hours: {crash_count}");
+        
+        if has_crashed {
+            crash_count += 1;
+            println!("VB-Updater: Crash Count in last 24 hours: {crash_count}");
+        } else {
+            has_crashed = true;
+        }
 
         if crash_count == REPLACE_WITH_STABLE_VERSION_FREQ {
             println!("VB-Updater: Replacing {PROCESS_NAME} with stable version...");
