e3e640aef4279846706921137d35c707a0321c04
Raahim Windows <raahim.home@gmail.com>
2022-09-26 18:04:17 -0700

    make special commands work for windows

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,23 @@ use std::io::{BufRead, BufReader};
 use std::process::{Command, Stdio};
 use std::time::{Instant, Duration};
 
-const PROCESS_NAME: &str = "vb-rec";
+const PROCESS_NAME: &str = "main.exe";
 const REPLACE_WITH_STABLE_VERSION_FREQ: u32 = 3;
 const REPLACE_WITH_STABLE_VERSION_DURATION: Duration = Duration::from_secs(3600 * 24);
+
+fn kill_process(id: u32) {
+    if cfg!(target_os = "unix") {
+        let _ = Command::new(format!("kill"))
+                    .args(["-9", &id.to_string()])
+                    .output()
+                    .expect("failed to execute process");
+    } else {
+         let _ = Command::new(format!("taskkill"))
+                    .args(["/F", "/PID", &id.to_string()])
+                    .output()
+                    .expect("failed to execute process");
+    }
+}
 
 fn replace_with_stable_version() {
     println!("replacing...");
@@ -19,10 +33,10 @@ fn main() {
     while should_run {
         println!("VB-Updater: Starting {PROCESS_NAME}\n");
 
-        let mut output = Command::new(format!("/Users/raahim/vb-updater-rust/src/vb-rec"))
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("failed to execute process");
+        let mut output = Command::new(format!("C:\\Users\\Administrator\\Desktop\\Dev\\vb-rec-v2-personal\\src\\main.exe"))
+                                    .stdout(Stdio::piped())
+                                    .spawn()
+                                    .expect("failed to execute process");
 
         let id = output.id();
 
@@ -37,7 +51,7 @@ fn main() {
 
             println!("{}", line);
 
-            if line == "RECORDER_RESTART_REQUESTE" {
+            if line == "RECORDER_RESTART_REQUESTED" {
                 println!("\nVB-Updater: RECORDER_RESTART_REQUESTED - Restarting VB-Recorder");
                 
                 // Resetting values
@@ -47,10 +61,7 @@ fn main() {
                 has_crashed = false;
 
                 // Killing process
-                let _ = Command::new(format!("kill"))
-                    .args(["-9", &id.to_string()])
-                    .output()
-                    .expect("failed to execute process");
+                kill_process(id);
             } else if line == "RECORDER_STOP_REQUESTED" {
                 println!("\nVB-Updater: RECORDER_STOP_REQUESTED - Stopping VB-Recorder");
 
@@ -58,10 +69,7 @@ fn main() {
                 has_crashed = false;
                 
                 // Killing process
-                let _ = Command::new(format!("kill"))
-                    .args(["-9", &id.to_string()])
-                    .output()
-                    .expect("failed to execute process");
+                kill_process(id);
             }
             
             // If duration since start has passed 24 hours, reset crash_count & start_time
